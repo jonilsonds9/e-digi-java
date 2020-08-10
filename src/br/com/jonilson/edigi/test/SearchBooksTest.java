e9011766a3e8c631f238ec29491096e0bcc4f8fe
jonilson <jonilsondeveloper@gmail.com>
2020-08-10 19:32:26 -0300

    Adicionando SearchBooksTest

---
+++ src/br/com/jonilson/edigi/test/SearchBooksTest.java
@@ -0,0 +1,58 @@
+package br.com.jonilson.edigi.test;
+
+import br.com.jonilson.edigi.dao.BookDao;
+import br.com.jonilson.edigi.model.Author;
+import br.com.jonilson.edigi.model.Book;
+import br.com.jonilson.edigi.model.Category;
+
+import java.util.List;
+
+public class SearchBooksTest {
+    public static void main(String[] args) {
+
+        try {
+            BookDao bookDao = new BookDao();
+
+            Author author = new Author("ana", "ana@gmail.com");
+            Category category = new Category("Programação");
+
+            Book book1 = new Book(
+                    "Aplicações web real-time com Node.js",
+                    "Node.js é uma poderosa plataforma. Ele permite escrever aplicações JavaScript no server-side, tirando proveito da sintaxe e familiaridade da linguagem para escrever aplicações web escaláveis. Como o Node.js usa um modelo orientado a eventos, focado em I/O não bloqueante, desenvolver nele pode ser diferente para quem está acostumado às aplicações web tradicionais. Neste livro, Caio Ribeiro Pereira quebra essa enorme barreira, mostrando claramente essa mudança de paradigma, além de focar em tópicos importantes, as APIs principais e frameworks como o Express e o Socket.IO.",
+                    "1 Bem-vindo ao mundo Node.js" +
+                            " 1.1 O problema das arquiteturas bloqueantes" +
+                            " 1.2 E assim nasceu o Node.js" +
+                            " 1.3 Single-thread",
+                    185,
+                    "978-85-66250-14-5",
+                    author,
+                    category,
+                    1,
+                    29.90
+            );
+
+            Book book2 = new Book(
+                    "Aplicações com PHP",
+                    "Node.js é uma poderosa plataforma. Ele permite escrever aplicações JavaScript no server-side, tirando proveito da sintaxe e familiaridade da linguagem para escrever aplicações web escaláveis. Como o Node.js usa um modelo orientado a eventos, focado em I/O não bloqueante, desenvolver nele pode ser diferente para quem está acostumado às aplicações web tradicionais. Neste livro, Caio Ribeiro Pereira quebra essa enorme barreira, mostrando claramente essa mudança de paradigma, além de focar em tópicos importantes, as APIs principais e frameworks como o Express e o Socket.IO.",
+                    "1 Bem-vindo ao mundo Node.js" +
+                            " 1.1 O problema das arquiteturas bloqueantes" +
+                            " 1.2 E assim nasceu o Node.js" +
+                            " 1.3 Single-thread",
+                    143,
+                    "978-85-66250-14-6",
+                    author,
+                    category,
+                    1,
+                    32.90
+            );
+
+            bookDao.add(book1);
+            bookDao.add(book2);
+
+            List<Book> result = bookDao.searchBooks("webteste");
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
